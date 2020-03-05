dec1130e791bf2c0182081649dd091e5f4cb8f79
khanhvu161188 <khanh.vu1611@outlook.com>
2020-03-05 22:34:37 +1000

    Update installBazel.sh

--- scripts/installBazel.sh
+++ scripts/installBazel.sh
@@ -7,11 +7,11 @@
 #
 INSTALL_DIR=$PWD
 cd $HOME
-wget https://github.com/bazelbuild/bazel/releases/download/2.2.0/bazel-2.2.0-dist.zip
-unzip bazel-2.2.0-dist.zip -d bazel-2.2.0-dist
+wget https://github.com/bazelbuild/bazel/releases/download/2.1.1/bazel-2.1.1-dist.zip
+unzip bazel-2.1.1-dist.zip -d bazel-2.1.1-dist
 
-sudo chmod -R ug+rwx $HOME/bazel-2.2.0-dist
+sudo chmod -R ug+rwx $HOME/bazel-2.1.1-dist
 # git clone https://github.com/bazelbuild/bazel.git
-cd bazel-2.2.0-dist
+cd bazel-2.1.1-dist
 ./compile.sh 
 sudo cp output/bazel /usr/local/bin
