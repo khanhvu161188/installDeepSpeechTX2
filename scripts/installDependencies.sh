693801464a7289fbccba8d30b3a945320e6faa8e
khanhvu161188 <khanh.vu1611@outlook.com>
2020-03-06 15:26:59 +1000

    Update installDependencies.sh

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -8,3 +8,10 @@ sudo apt-get install build-essential openjdk-8-jdk python zip unzip -y
 sudo apt-get install zip unzip autoconf automake libtool curl zlib1g-dev maven -y
 # Install Python 3.x
 sudo apt-get install python3-numpy python3-dev python3-pip python3-wheel
+
+sudo apt-get install libsox-dev
+sudo apt-get install python3-setuptools
+sudo apt-get install python-setuptools python-wheel
+
+sudo pip3 install wheel
+sudo pip install wheel
